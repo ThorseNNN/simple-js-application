3975bdc9e6cf1ceec0b7817ff636c38df922558d
ThorseNNN <114915033+ThorseNNN@users.noreply.github.com>
2022-11-01 14:45:21 +0700

    Initial Pokemon Characters

--- js/script.js
+++ js/script.js
@@ -7,15 +7,15 @@ let pokemonList = [
 
 
     {
-        name: 'Metapod',
-        height: .7,
-        type: ['bug','shed skin']
+        name: 'Butterfree',
+        height: 1.1,
+        type: ['bug','compoundeyes','tintedlens']
     },
 
     {
         name: 'Metapod',
-        height: .7,
-        type: ['bug','shed skin']
+        height: 1,
+        type: ['bug','swarm','sniper']
     }
 ]
 
